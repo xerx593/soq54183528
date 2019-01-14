16cadb85e799e5d986a2fda1b7dfa0d91df158ad
xerx593 <alexlutz79@gmail.com>
2019-01-14 17:02:07 +0100

    cannot reproduce

---
+++ src/main/java/inlinecompiler/InlineCompiler.java
@@ -0,0 +1,106 @@
+package inlinecompiler;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+
+public class InlineCompiler {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(64);
+        sb.append("package testcompile;\n");
+        // a little "more common" external lib
+        sb.append("import org.apache.commons.lang3.StringUtils;\n");
+        sb.append("public class HelloWorld implements inlinecompiler.InlineCompiler.DoStuff {\n");
+        sb.append("    public void doStuff() {\n");
+        sb.append("        System.out.println(\"Hello world\");\n");
+        sb.append("        System.out.println(StringUtils.isNotBlank(\"Hello world\"));\n");
+        sb.append("    }\n");
+        sb.append("}\n");
+
+        File helloWorldJava = new File("testcompile/HelloWorld.java");
+        if (helloWorldJava.getParentFile().exists() || helloWorldJava.getParentFile().mkdirs()) {
+
+            try {
+                Writer writer = null;
+                try {
+                    writer = new FileWriter(helloWorldJava);
+                    writer.write(sb.toString());
+                    writer.flush();
+                } finally {
+                    try {
+                        writer.close();
+                    } catch (IOException e) {
+                    }
+                }
+
+                /** Compilation Requirements *********************************************************************************************/
+                DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+                JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+                // in "mvn exec:run" (Netbeans Run), don't need this:
+                try (StandardJavaFileManager fileManager = compiler.getStandardFileManager(diagnostics, null, null)) {
+                    // in "mvn exec:run" (Netbeans Run), don't need this:
+                    List<String> optionList = new ArrayList<>();
+                    // optionList.add("-classpath");
+                    // optionList.add(System.getProperty("java.class.path") + ";sikuli.jar");
+                    
+                    Iterable<? extends JavaFileObject> compilationUnit
+                            = fileManager.getJavaFileObjectsFromFiles(Arrays.asList(helloWorldJava));
+                    JavaCompiler.CompilationTask task = compiler.getTask(
+                            null,
+                            fileManager,
+                            diagnostics,
+                            optionList,
+                            null,
+                            compilationUnit);
+                    /********************************************************************************************* Compilation Requirements **/
+                    if (task.call()) {
+                        /** Load and execute *************************************************************************************************/
+                        System.out.println("Yipe");
+                        // Create a new custom class loader, pointing to the directory that contains the compiled
+                        // classes, this should point to the top of the package structure!
+                        URLClassLoader classLoader = new URLClassLoader(new URL[]{new File("./").toURI().toURL()});
+                        // Load the class from the classloader by name....
+                        Class<?> loadedClass = classLoader.loadClass("testcompile.HelloWorld");
+                        // Create a new instance...
+                        Object obj = loadedClass.newInstance();
+                        // Santity check
+                        if (obj instanceof DoStuff) {
+                            // Cast to the DoStuff interface
+                            DoStuff stuffToDo = (DoStuff)obj;
+                            // Run it baby
+                            stuffToDo.doStuff();
+                        }
+                        /************************************************************************************************* Load and execute **/
+                    } else {
+                        diagnostics.getDiagnostics().forEach((diagnostic) -> {
+                            System.out.format("Error on line %d in %s%n",
+                                    diagnostic.getLineNumber(),
+                                    diagnostic.getSource().toUri());
+                        });
+                    }
+                }
+                // optionList.add("-classpath");
+                // optionList.add(System.getProperty("java.class.path") + ";sikuli.jar");
+            } catch (IOException | ClassNotFoundException | InstantiationException | IllegalAccessException exp) {
+                exp.printStackTrace();
+            }
+        }
+    }
+
+    public interface DoStuff {
+
+        void doStuff();
+    }
+
+}
